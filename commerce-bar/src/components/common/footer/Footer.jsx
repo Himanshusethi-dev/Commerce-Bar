50f036ca179f821e7e15b56abd6dd803860a57e9
Himanshusethi-dev <himanshusethi9641@gmail.com>
2024-08-04 15:15:53 +0530

    Footer code

--- commerce-bar/src/components/common/footer/Footer.jsx
+++ commerce-bar/src/components/common/footer/Footer.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
+import './footer.css'
+import useNavigationFetcher from '../../../hooks/useNavigationFetcher';
+import FooterItem from './FooterItem';
 
 const Footer = () => {
+  const footer = useNavigationFetcher('footer')
+  const [footerOptions, setFooterOptions] = useState([]);
+
+  useEffect(() => {
+    if (footer) {
+      const { items } = footer
+      setFooterOptions(items);
+    }
+
+  }, [footer])
+
+  useEffect(() => {
+    console.log("footerOptions", footerOptions);
+  }, [footerOptions])
+
   return (
     <>
-        <Box />
+      <div className="container">
+        <Box component="div" justifyContent={'center'}  sx={{display:'flex',borderTop: 1,paddingTop:'10px'}} >
+          {
+            footerOptions && (
+              footerOptions.map((item,i)=>(
+                <Fragment  key={i} >
+                  <Box component="div"  sx={{ width:'30%' }} >
+                  <FooterItem  data={item}  >
+                   </FooterItem>
+                  </Box>
+                </Fragment>
+              ))
+            )
+          }
+        </Box>
+      </div>
     </>
   )
 }
